50e5dedd64301264823cb5b6ba12e4c1bd249e89
dhruvv <dhruva.venkataraman@gmail.com>
2016-05-20 15:55:25 +0530

    Add files via upload

---
+++ ytd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import tkinter
+import os
+
+window = tkinter.Tk()
+window.geometry("300x300")
+window.configure(bg="blue")
+#window.wm_iconbitmap('/home/icon.ico')
+window.title("Youtube Downloader")
+def doDownload() :
+	url = ent.get()
+	os.system("youtube-dl "+url)
+	os.system("sudo cp *.mkv /mnt/DOS/Dhruv/Videos/Downloaded/MKV")
+img = tkinter.PhotoImage(file="logo.gif")
+lb = tkinter.Label(window, image=img)
+lb.pack()
+
+
+lbl1 = tkinter.Label(window, text="Download your video now!", fg="White",bg="black")
+lbl1.pack()
+
+ent = tkinter.Entry(window)
+ent.pack()
+
+btn = tkinter.Button(text="Download Now" , command=doDownload,)
+btn.pack()
+
+window.mainloop()
+
